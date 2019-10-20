2018e9eea74cdd03b30ca5bfbca4a6b332f47244
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-20 09:36:44 +0530

    Initial commit

---
+++ app/src/main/java/com/piyushmaheswari/bluecanary/MainActivity.java
@@ -0,0 +1,31 @@
+package com.piyushmaheswari.bluecanary;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView =findViewById(R.id.webView1);
+        webView.loadUrl("http://m.facebook.com");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyAppWebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
